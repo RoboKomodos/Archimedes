3cc8b9391f0b455c7c4175303cc4830a30bd16d3
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2019-09-25 15:42:48 -0500

    Create OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,23 +7,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  Joystick logitech = new Joystick(0);
+  public OI(){
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
-
+  }
   //// TRIGGERING COMMANDS WITH BUTTONS
   // Once you have a button, it's trivial to bind it to a button in one of
   // three ways:
