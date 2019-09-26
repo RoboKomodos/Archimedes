eac8ea8dfecd54240096f8ce08be83245a412a32
Connor Gannaway <44012197+cgannaway@users.noreply.github.com>
2019-09-26 09:27:43 -0500

    v1.0

--- src/main/java/frc/robot/commands/runActuator.java
+++ src/main/java/frc/robot/commands/runActuator.java
@@ -7,6 +7,7 @@ import frc.robot.Robot;
  * An example command.  You can replace me with your own command.
  */
 public class runActuator extends Command {
+  private boolean done;
   public runActuator() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_actuator);
@@ -15,17 +16,21 @@ public class runActuator extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    done = true;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.m_actuator.runActuator();
+    done = true;
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return done;
+    
   }
 
   // Called once after isFinished returns true
