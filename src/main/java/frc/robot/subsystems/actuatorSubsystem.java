c980d4d1215298b3070ec12157e85b7c17fe95fe
Connor Gannaway <44012197+cgannaway@users.noreply.github.com>
2019-09-26 08:25:51 -0500

    v1.0

--- src/main/java/frc/robot/subsystems/actuatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/actuatorSubsystem.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.Victor;
+
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
@@ -8,10 +11,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class actuatorSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  private Victor actuator;
 
+  public actuatorSubsystem(){
+    actuator = new Victor(RobotMap.actuator);
+  }
+
+  public void runActuator(){
+    actuator.setPosition(0.3);
+    actuator.setPosition(0.0);
+  }
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void initDefaultCommand(){
+
   }
 }
--- src/main/java/frc/robot/subsystems/launchSubsystem.java
+++ src/main/java/frc/robot/subsystems/launchSubsystem.java
@@ -34,6 +34,5 @@ public void stopMotors(){
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
-    setDefaultCommand(new startMotors());
   }
 }
