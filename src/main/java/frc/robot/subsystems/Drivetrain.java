0bb36634985618bbf5f5efdde0ea2f81372bca1a
C0de5mith <lucidanonlucy@gmail.com>
2019-09-27 23:47:51 -0500

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,13 +1,37 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 public class Drivetrain extends Subsystem {
+    private Victor leftMotor;
+    private Victor rightMotor;
+    public Drivetrain(){
+        leftMotor = new Victor(RobotMap.leftDrive);
+        rightMotor = new Victor(RobotMap.rightDrive);
+    }
 
     @Override
     protected void initDefaultCommand() {
-        // TODO Auto-generated method stub
+        
 
     }
-    
+
+    public void setSpeed(double leftSpeed, double rightSpeed){
+        leftMotor.set(leftSpeed);
+        rightMotor.set(rightSpeed);
+    }
+    /** 
+     * Joystick to Drivetrain formula
+     *  Accepts values from -1 to 1
+     *  http://home.kendra.com/mauser/joystick.html
+     */
+    public void setJoystickPosition(double dx, double dy){
+        dx*=-1;
+        double v=(2-Math.abs(dx))*dy;
+        double w=(2-Math.abs(dy))*dx;
+        setSpeed((v-w)/2,(v+w)/2);
+    }
+
 }
