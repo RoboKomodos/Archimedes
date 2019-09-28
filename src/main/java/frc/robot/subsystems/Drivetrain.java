1e5a36fc67ce510af5486e279547e1140755c709
C0de5mith <lucidanonlucy@gmail.com>
2019-09-27 23:09:26 -0500

    Create Drivetrain.java

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
