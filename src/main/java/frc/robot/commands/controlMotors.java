680f61602f4bd479cffab43fb1bddde4b5a5ff90
Connor Gannaway <44012197+cgannaway@users.noreply.github.com>
2019-09-30 08:53:22 -0500

    finished launch

---
+++ src/main/java/frc/robot/commands/controlMotors.java
@@ -0,0 +1,57 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+/**
+ * An example command.  You can replace me with your own command.
+ */
+public class controlMotors extends Command {
+    private boolean areMotorsOn;
+    private boolean done = false;
+  public controlMotors() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.m_launch);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+      areMotorsOn = Robot.m_launch.areMotorsRunning();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(areMotorsOn == false){
+        Robot.m_launch.startMotors();
+        done = true;
+    }
+    else{
+        Robot.m_launch.stopMotors();
+        done = true;
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    if(done == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- src/main/java/frc/robot/subsystems/launchSubsystem.java
+++ src/main/java/frc/robot/subsystems/launchSubsystem.java
@@ -2,13 +2,37 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 
 public class launchSubsystem extends Subsystem {
 private Victor motor1;
 private Victor motor2;
 
+public launchSubsystem(){
+  motor1 = new Victor(RobotMap.launchMotor1);
+  motor2 = new Victor(RobotMap.launchMotor2);
+}
 
+public void startMotors(){
+  motor1.set(0.5);
+  motor2.set(0.5);
+}
+
+public void stopMotors(){
+  motor1.set(0.0);
+  motor2.set(0.0);
+}
+
+public boolean areMotorsRunning(){
+  double speed = motor1.getSpeed();
+  if(speed == 0.0){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
 
   @Override
   public void initDefaultCommand() {
