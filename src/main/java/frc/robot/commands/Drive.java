85577ab0a01dc1c7a583a31e176d93f791415ebc
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2019-10-02 15:44:30 -0500

    Update Drive.java

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -11,9 +11,11 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class Drive extends Command {
-  public Drive() {
+
+public Drive() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.m_drivetrain);
   }
 
   // Called just before this Command runs the first time
