b54bf5f3743a9ba84c065d1800975cf7aad4e63f
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2019-10-02 15:28:13 -0500

    Create drivetrain subsystem and drive command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,6 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot {
   public static OI m_oi;
+  public static Drivetrain m_drivetrain;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -33,6 +35,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
+    m_drivetrain = new Drivetrain();
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.Drive;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private Victor leftMotor;
+  private Victor rightMotor;
+  public Drivetrain(){
+    leftMotor=new Victor(RobotMap.leftDrive);
+    rightMotor=new Victor(RobotMap.rightDrive);
+  }
+
+  public void setSpeed(double left, double right){
+    leftMotor.set(left);
+    rightMotor.set(right);
+  }
+
+  /** 
+  *  Joystick to Drivetrain formula
+  *  Accepts values from -1 to 1
+  *  http://home.kendra.com/mauser/joystick.html
+  */
+  public void setJoystickPosition(double dx, double dy){
+    dx*=-1;
+    double v=(2-Math.abs(dx))*dy;
+    double w=(2-Math.abs(dy))*dx;
+    setSpeed((v-w)/2,(v+w)/2);
+  }
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new Drive());
+  }
+}
