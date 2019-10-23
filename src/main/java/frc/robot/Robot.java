fd38539b8bfd349dd12b0f675b6c4e9ca284b558
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2019-10-23 15:11:18 -0500

    Move drivetrain initialization

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,8 +34,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_oi = new OI();
     m_drivetrain = new Drivetrain();
+    m_oi = new OI();
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
