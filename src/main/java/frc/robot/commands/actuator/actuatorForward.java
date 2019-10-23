27485fa834d6e458a89dea14af5f8cba036d1c39
Connor Gannaway <ccgannaway@icloud.com>
2019-10-23 13:48:26 -0700

    actuator stuff

---
+++ src/main/java/frc/robot/commands/actuator/actuatorForward.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.actuator;
+
+import edu.wpi.first.wpilibj.command.TimedCommand;
+import frc.robot.Robot;
+import frc.robot.subsystems.actuatorSubsystem;
+
+/**
+ * Add your docs here.
+ */
+public class actuatorForward extends TimedCommand {
+  /**
+   * Add your docs here.
+   */
+  public actuatorForward(double timeout) {
+    super(timeout);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.m_actuator);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.m_actuator.runMotor();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Called once after timeout
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- src/main/java/frc/robot/subsystems/launchSubsystem.java
+++ src/main/java/frc/robot/subsystems/launchSubsystem.java
@@ -25,13 +25,6 @@ public class launchSubsystem extends Subsystem {
   }
 
   public boolean areMotorsRunning(){
-    /*double speed = motor1.getSpeed();
-    if(speed == 0.0){
-      return false;
-    }
-    else{
-      return true;
-    }*/
     return motor1.getSpeed() == 0.0 ? false : true;
   }
   
